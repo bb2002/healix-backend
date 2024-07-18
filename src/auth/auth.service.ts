ebddc1686c064d55fe75642923fe3599a3192f7f
KMWH <kmwh030@gmail.com>
2024-07-18 22:37:15 +0900

    google login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import LoginSuccessDto from './dto/login-success.dto';
+import LoginProvider from './enums/LoginProvider';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async signInWithGoogle(user: any): Promise<LoginSuccessDto> {
+    const loginSuccessDto: LoginSuccessDto = {
+      provider: LoginProvider.GOOGLE,
+      uniqueId: user.email,
+      name: `${user.firstName} ${user.lastName}`,
+      profileImageUrl: user.photo,
+    };
+    return loginSuccessDto;
+  }
+}
