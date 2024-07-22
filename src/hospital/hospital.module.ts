6e643afc71fbb10915cbfc389298218917aa1e83
KMWH <kmwh030@gmail.com>
2024-07-22 23:02:49 +0900

    controller 삭제

--- src/hospital/hospital.module.ts
+++ src/hospital/hospital.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HospitalService } from './hospital.service';
-import { HospitalController } from './hospital.controller';
 
 @Module({
   providers: [HospitalService],
-  controllers: [HospitalController],
 })
 export class HospitalModule {}
