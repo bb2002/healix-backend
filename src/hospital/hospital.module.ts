47f6aa8b66404e0f679025a3a1988c937bfb2f15
Ballbot <5252bb@daum.net>
2024-07-23 19:43:15 +0900

    :hammer: buf gix

--- src/hospital/hospital.module.ts
+++ src/hospital/hospital.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HospitalService } from './hospital.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import HospitalEntity from './entities/hospital.entity';
 
 @Module({
   providers: [HospitalService],
+  imports: [TypeOrmModule.forFeature([HospitalEntity])],
 })
 export class HospitalModule {}
