0363e14c56bd180acc46dc90c866ff703fda3c8f
KMWH <kmwh030@gmail.com>
2024-07-24 22:27:07 +0900

    Update hospital.module.ts

--- src/hospital/hospital.module.ts
+++ src/hospital/hospital.module.ts
@@ -3,9 +3,18 @@ import { HospitalService } from './hospital.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import HospitalEntity from './entities/hospital.entity';
 import { AppointmentModule } from 'src/appointment/appointment.module';
+import { HospitalController } from './hospital.controller';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [HospitalService],
-  imports: [TypeOrmModule.forFeature([HospitalEntity]), AppointmentModule],
+  controllers: [HospitalController],
+  imports: [
+    TypeOrmModule.forFeature([HospitalEntity]),
+    AppointmentModule,
+    UserModule,
+    JwtModule,
+  ],
 })
 export class HospitalModule {}
