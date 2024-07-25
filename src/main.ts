054dd8087f493571434d3204238540ce6f4ea6cd
Ballbot <5252bb@daum.net>
2024-07-25 11:41:42 +0900

    :hammer: enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(
     ['/docs'],
     expressBasicAuth({
